7f4568997f24a26e534ad9a2a63e6af9b5a006d0
Yang Tun-Kai <ks97121@gmail.com>
2020-05-12 23:50:17 +0800

    Update color extension

--- Kaohsiung City Bike/Extension/UIColor+Extension.swift
+++ Kaohsiung City Bike/Extension/UIColor+Extension.swift
@@ -47,6 +47,24 @@ extension UIColor {
         }
     }()
     
+    static let stopColor: UIColor = {
+        if #available(iOS 13, *) {
+            return UIColor { (UITraitCollection: UITraitCollection) -> UIColor in
+                if UITraitCollection.userInterfaceStyle == .dark {
+                    // Return the color for Dark Mode
+                    return UIColor(hex: 0x993d5b) ?? .clear
+                    
+                } else {
+                    // Return the color for Light Mode
+                    return UIColor(hex: 0xFF6699) ?? .clear
+                }
+            }
+        } else {
+            // Return a fallback color for iOS 12 and lower.
+            return UIColor(hex: 0xFF6699) ?? .clear
+        }
+    }()
+    
     static let titleColor: UIColor = {
         if #available(iOS 13, *) {
             return .secondaryLabel
